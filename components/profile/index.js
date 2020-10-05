41f106127cf20f7078b6c4d41dea4596bdfaa03a
pvijayvergiya <Prateek.Vijayvergiya2@mindtree.com>
2020-10-05 17:50:32 +0530

    assigned default values

--- components/profile/index.js
+++ components/profile/index.js
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography'
 import styles from '../../styles/Profile.module.css'
 
 const Profile = ({userData}) => {
-  const { phone, firstName, lastName, picture, dateOfBirth, gender, email, location: {city, street, country}} = userData
+  const {title='mr', phone='1234567890', firstName, lastName, picture, dateOfBirth, gender, email, location: {city, street, country}} = userData
   return (
     <Box>
       <Grid container>
@@ -15,7 +15,7 @@ const Profile = ({userData}) => {
         <Grid item container lg={4} xs={12} className={styles.container}>
           <Grid item>
             <Typography variant={'h4'}>
-              {`${firstName} ${lastName}`}
+              {`${title} ${firstName} ${lastName}`}
             </Typography>
             <Typography variant={'h6'}>Gender: {gender}</Typography>
             <Typography variant={'h6'}>Date Of Birth: {dateOfBirth}</Typography>
